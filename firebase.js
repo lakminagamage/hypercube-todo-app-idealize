859d7ac56a5808aeafb70626f6f999aeecb9f59e
Lakmina Pramodya Gamage <pramodyalakmina@gmail.com>
2023-06-17 09:34:47 +0530

    added firebase config and recat navigation

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCf9L6BA7OLSIDoiic_xRtfgXJ1L2iHpNA",
+  authDomain: "hc-todo-app-a5d7e.firebaseapp.com",
+  projectId: "hc-todo-app-a5d7e",
+  storageBucket: "hc-todo-app-a5d7e.appspot.com",
+  messagingSenderId: "552585390298",
+  appId: "1:552585390298:web:8cb0c5e351ab7c57171de3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
