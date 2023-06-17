3f76c1a51828f2041647dd30af3062e95b73219d
Sankha Bimsara <sankha.b21@gmail.com>
2023-06-17 10:11:24 +0530

    revised colors

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SplashScreen from './screens/SplashScreen';
+//import SplashScreen from './screens/SplashScreen';
 import HomeScreen from './screens/HomeScreen';
-
-//import MqttClient from './mqttClient.js'
-// import Paho from 'paho-mqtt';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,8 +10,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options={{headerShown:false}} name="Splash" component={SplashScreen} />
         <Stack.Screen options={{headerShown:false}} name="Home" component={HomeScreen} />
+        {/*<Stack.Screen options={{headerShown:false}} name="Splash" component={SplashScreen} />*/}
+       
         
       </Stack.Navigator>
     </NavigationContainer>
